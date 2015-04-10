1105de22249e81fbd12e83ea53a6c72be56c4104
ntsmwk <markus.weissenbek@gmx.at>
2015-04-10 16:08:13 +0200

    added capabilities, method struct

--- Agent/agent.c
+++ Agent/agent.c
@@ -4,6 +4,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+
+
+struct {
+	char* methodName, 
+	methodTiming* caller,
+	long selftime,
+	long wallclocktime
+}methodTiming;
+
+char* clazz;
+char* method;
+char* param;
+char* ret;
+jmethodID method_id = NULL;
+struct methodTiming* methodArray;
+int methodCount = 0;
+
 void JNICALL OnVMInit(jvmtiEnv *jvmti, JNIEnv* jni, jthread thread) {
 	jvmtiStackInfo *stack_info;
 	jint thread_count;
@@ -34,6 +51,32 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* _vm, char* options, void* reserved) 
 	jvmtiEnv* jvmti = NULL;
 	(*_vm)->GetEnv(_vm, (void**) &jvmti, JVMTI_VERSION);
 
+	
+	if (options != NULL && options[0] != '\0') {
+		char* nextArgument = options;
+		const char delimiters[] = ":=";
+		char *key, *value;
+		int i = 0;
+
+		do {
+			if(i==0) key = strtok(nextArgument, delimiters);
+			else key = strtok(NULL, delimiters);
+
+			if(key != NULL){
+				value = strtok(NULL, delimiters);
+				if(strstr(key, "clazz")!=NULL) clazz = value;
+				else if(strstr(key, "method")!=NULL) method = value;
+				else if(strstr(key, "param")!=NULL) param = value;
+				else if(strstr(key, "ret")!=NULL) ret = value;
+			}
+
+			i++;
+		} while(key != NULL); 
+	}
+	
+	
+	
+	
 	jvmtiError error;
 	jvmtiCapabilities requestedCapabilities, potentialCapabilities;
 	memset(&requestedCapabilities, 0, sizeof(requestedCapabilities));
@@ -62,12 +105,35 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* _vm, char* options, void* reserved) 
 	jvmtiEventCallbacks callbacks;
 	memset(&callbacks, 0, sizeof(callbacks));
 	callbacks.VMInit = OnVMInit;
+	callbacks.MethodEntry = OnMethodEntry;
+	callbacks.MethodExit = OnMethodExit;
 
 	(*jvmti)->SetEventCallbacks(jvmti, &callbacks, sizeof(callbacks));
 	(*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, NULL);
+	(*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL);
+	(*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_EXIT, NULL);
 	return 0;
 }
 
 JNIEXPORT void JNICALL Agent_UnLoad() {
 	// print 10 hottest methods
+}
+
+void JNICALL OnMethodEntry(jvmtiEnv *jvmti, JNIEnv* jni, jthread thread, jmethodID method)
+{
+	if(method == method_id)
+	{
+		//Take time
+		//add to struct
+	}
+	
+}
+
+void JNICALL OnMethodExit(jvmtiEnv *jvmti, JNIEnv* jni, jthread thread, jmethodID method, jboolean was_popped_by_exception, jvalue return_value)
+{
+	if(method == method_id)
+	{
+		//Take time
+		//take time in struct
+	}
 }
